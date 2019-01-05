d541825219db2bbdf8b1f5164b2e4951f9f21977
aaron <afcofiel@umich.edu>
2019-01-05 15:52:25 -0500

    Added constructor so test case will run

--- src/pure_pursuit.cpp
+++ src/pure_pursuit.cpp
@@ -1,7 +1,8 @@
 // implement class here
 #include "pure_pursuit.h"
 
-PurePursuit::PurePursuit(const Path &robot_path, const double &lookahead_distance) {}
+PurePursuit::PurePursuit(const Path &robot_path, const double &lookahead_distance): m_robot_path(robot_path), m_lookahead_distance(lookahead_distance) {}
+
 
 Point2D PurePursuit::get_target_state(const Point3D &state) {}
 
@@ -18,38 +19,38 @@ Point3D PurePursuit::get_point_on_path(const double &position) {}
 std::pair<Point2D, double> PurePursuit::get_location_on_path(const Point2D &state) {
     //Seach for the shortest distance from the robot to each path segment
     double counter = 0;
-    double slope, distanceTraveled, distanceToRobot, shortestDistance = std::numeric_limits<int>::max();
+    double slope, distanceTraveled, distanceToRobot, shortestDistance = INT16_MAX;
     Point2D closestLocation, currentLoc;
-    closestLocation.x = m_robot_path[0].x;
-    closestLocation.y = m_robot_path[0].y;
-    currentLoc.x = m_robot_path[0].x;
-    currentLoc.y = m_robot_path[0].y;
+    closestLocation.x = m_robot_path.at(0).x;
+    closestLocation.y = m_robot_path.at(0).y;
+    currentLoc.x = m_robot_path.at(0).x;
+    currentLoc.y = m_robot_path.at(0).y;
 
-    for (int i = 0; i < (m_robot_path.size() - 1); i++) {
+    for (ulong i = 0; i < (m_robot_path.size() - 1); i++) {
 
-        slope = ((m_robot_path[i + 1].y - m_robot_path[i].y) / (m_robot_path[i + 1].x - m_robot_path[i].x));
+        slope = ((m_robot_path.at(i + 1).y - m_robot_path.at(i).y) / (m_robot_path.at(i + 1).x - m_robot_path.at(i).x));
         counter = 0;
 
-        while ((currentLoc.x != m_robot_path[i + 1].x) || (currentLoc.y != m_robot_path[i + 1].y)) {
+        while ((currentLoc.x != m_robot_path.at(i + 1).x) || (currentLoc.y != m_robot_path.at(i + 1).y)) {
             //segment is going up and right
-            if ((m_robot_path[i].y < m_robot_path[i + 1].y) && (m_robot_path[i].x < m_robot_path[i + 1].x)) {
+            if ((m_robot_path.at(i).y < m_robot_path.at(i + 1).y) && (m_robot_path.at(i).x < m_robot_path.at(i + 1).x)) {
                 counter++;
 
-                currentLoc.x = (m_robot_path[i].x + counter);
+                currentLoc.x = (m_robot_path.at(i).x + counter);
                 currentLoc.y = (currentLoc.y + slope);
 
                 //if currentLocation goes beyond next point, it is set to the next point
-                if (currentLoc.x >= m_robot_path[i + 1].x) {
-                    currentLoc.x = m_robot_path[i + 1].x;
-                    currentLoc.y = m_robot_path[i + 1].y;
+                if (currentLoc.x >= m_robot_path.at(i + 1).x) {
+                    currentLoc.x = m_robot_path.at(i + 1).x;
+                    currentLoc.y = m_robot_path.at(i + 1).y;
                 }
-                if (currentLoc.y >= m_robot_path[i + 1].y) {
-                    currentLoc.x = m_robot_path[i + 1].x;
-                    currentLoc.y = m_robot_path[i + 1].y;
+                if (currentLoc.y >= m_robot_path.at(i + 1).y) {
+                    currentLoc.x = m_robot_path.at(i + 1).x;
+                    currentLoc.y = m_robot_path.at(i + 1).y;
                 }
 
                 //calculates distance to robot
-                distanceToRobot = std::sqrt(std::pow(std::abs(-currentLoc.x + state.x), 2) + std::pow(std::abs(-currentLoc.y + state.y), 2));
+                distanceToRobot = sqrt(std::pow(std::fabs(-currentLoc.x + state.x), 2) + std::pow(std::fabs(-currentLoc.y + state.y), 2));
 
                 //checks to see if currentLocation is closer to the current position
                 if (distanceToRobot < shortestDistance) {
@@ -58,24 +59,24 @@ std::pair<Point2D, double> PurePursuit::get_location_on_path(const Point2D &stat
                 }
             }
                 //segment is going up and to the left
-            else if ((m_robot_path[i].y < m_robot_path[i + 1].y) && (m_robot_path[i].x > m_robot_path[i + 1].x)) {
+            else if ((m_robot_path.at(i).y < m_robot_path.at(i + 1).y) && (m_robot_path.at(i).x > m_robot_path.at(i + 1).x)) {
                 counter--;
 
-                currentLoc.x = (m_robot_path[i].x + counter);
+                currentLoc.x = (m_robot_path.at(i).x + counter);
                 currentLoc.y = (currentLoc.y + -slope);
 
                 //if currentLocation goes beyond next point, it is set to the next point
-                if (currentLoc.x <= m_robot_path[i + 1].x) {
-                    currentLoc.x = m_robot_path[i + 1].x;
-                    currentLoc.y = m_robot_path[i + 1].y;
+                if (currentLoc.x <= m_robot_path.at(i + 1).x) {
+                    currentLoc.x = m_robot_path.at(i + 1).x;
+                    currentLoc.y = m_robot_path.at(i + 1).y;
                 }
-                if (currentLoc.y >= m_robot_path[i + 1].y) {
-                    currentLoc.x = m_robot_path[i + 1].x;
-                    currentLoc.y = m_robot_path[i + 1].y;
+                if (currentLoc.y >= m_robot_path.at(i + 1).y) {
+                    currentLoc.x = m_robot_path.at(i + 1).x;
+                    currentLoc.y = m_robot_path.at(i + 1).y;
                 }
 
                 //calculates distance to robot
-                distanceToRobot = sqrt(pow(abs(-currentLoc.x + state.x), 2) + pow(abs(-currentLoc.y + state.y), 2));
+                distanceToRobot = sqrt(std::pow(std::fabs(-currentLoc.x + state.x), 2) + std::pow(std::fabs(-currentLoc.y + state.y), 2));
 
                 //checks to see if currentLocation is closer to the current position
                 if (distanceToRobot < shortestDistance) {
@@ -84,24 +85,24 @@ std::pair<Point2D, double> PurePursuit::get_location_on_path(const Point2D &stat
                 }
             }
                 //segment is going down and to the left
-            else if ((m_robot_path[i].y > m_robot_path[i + 1].y) && (m_robot_path[i].x > m_robot_path[i + 1].x)) {
+            else if ((m_robot_path.at(i).y > m_robot_path.at(i + 1).y) && (m_robot_path.at(i).x > m_robot_path.at(i + 1).x)) {
                 counter--;
 
-                currentLoc.x = (m_robot_path[i].x + counter);
+                currentLoc.x = (m_robot_path.at(i).x + counter);
                 currentLoc.y = (currentLoc.y + -slope);
 
                 //if currentLocation goes beyond next point, it is set to the next point
-                if (currentLoc.x <= m_robot_path[i + 1].x) {
-                    currentLoc.x = m_robot_path[i + 1].x;
-                    currentLoc.y = m_robot_path[i + 1].y;
+                if (currentLoc.x <= m_robot_path.at(i + 1).x) {
+                    currentLoc.x = m_robot_path.at(i + 1).x;
+                    currentLoc.y = m_robot_path.at(i + 1).y;
                 }
-                if (currentLoc.y <= m_robot_path[i + 1].y) {
-                    currentLoc.x = m_robot_path[i + 1].x;
-                    currentLoc.y = m_robot_path[i + 1].y;
+                if (currentLoc.y <= m_robot_path.at(i + 1).y) {
+                    currentLoc.x = m_robot_path.at(i + 1).x;
+                    currentLoc.y = m_robot_path.at(i + 1).y;
                 }
 
                 //calculates distance to robot
-                distanceToRobot = sqrt(pow(abs(-currentLoc.x + state.x), 2) + pow(abs(-currentLoc.y + state.y), 2));
+                distanceToRobot = sqrt(std::pow(std::fabs(-currentLoc.x + state.x), 2) + std::pow(std::fabs(-currentLoc.y + state.y), 2));
 
                 //checks to see if currentLocation is closer to the current position
                 if (distanceToRobot < shortestDistance) {
@@ -110,24 +111,24 @@ std::pair<Point2D, double> PurePursuit::get_location_on_path(const Point2D &stat
                 }
             }
                 //segment is going down and to the right
-            else if ((m_robot_path[i].y > m_robot_path[i + 1].y) && (m_robot_path[i].x < m_robot_path[i + 1].x)) {
+            else if ((m_robot_path.at(i).y > m_robot_path.at(i + 1).y) && (m_robot_path.at(i).x < m_robot_path.at(i + 1).x)) {
                 counter++;
 
-                currentLoc.x = (m_robot_path[i].x + counter);
+                currentLoc.x = (m_robot_path.at(i).x + counter);
                 currentLoc.y = (currentLoc.y + slope);
 
                 //if currentLocation goes beyond next point, it is set to the next point
-                if (currentLoc.x >= m_robot_path[i + 1].x) {
-                    currentLoc.x = m_robot_path[i + 1].x;
-                    currentLoc.y = m_robot_path[i + 1].y;
+                if (currentLoc.x >= m_robot_path.at(i + 1).x) {
+                    currentLoc.x = m_robot_path.at(i + 1).x;
+                    currentLoc.y = m_robot_path.at(i + 1).y;
                 }
-                if (currentLoc.y <= m_robot_path[i + 1].y) {
-                    currentLoc.x = m_robot_path[i + 1].x;
-                    currentLoc.y = m_robot_path[i + 1].y;
+                if (currentLoc.y <= m_robot_path.at(i + 1).y) {
+                    currentLoc.x = m_robot_path.at(i + 1).x;
+                    currentLoc.y = m_robot_path.at(i + 1).y;
                 }
 
                 //calculates distance to robot
-                distanceToRobot = sqrt(pow(abs(-currentLoc.x + state.x), 2) + pow(abs(-currentLoc.y + state.y), 2));
+                distanceToRobot = sqrt(std::pow(std::fabs(-currentLoc.x + state.x), 2) + std::pow(std::fabs(-currentLoc.y + state.y), 2));
 
                 //checks to see if currentLocation is closer to the current position
                 if (distanceToRobot < shortestDistance) {
@@ -136,18 +137,18 @@ std::pair<Point2D, double> PurePursuit::get_location_on_path(const Point2D &stat
                 }
             }
                 //segment is going up
-            else if ((m_robot_path[i].y < m_robot_path[i + 1].y) && (m_robot_path[i].x == m_robot_path[i + 1].x)) {
+            else if ((m_robot_path.at(i).y < m_robot_path.at(i + 1).y) && (m_robot_path.at(i).x == m_robot_path.at(i + 1).x)) {
                 counter++;
 
                 currentLoc.y = (currentLoc.y + counter);
 
                 //if currentLocation goes beyond next point, it is set to the next point
-                if (currentLoc.y >= m_robot_path[i + 1].y) {
-                    currentLoc.y = m_robot_path[i + 1].y;
+                if (currentLoc.y >= m_robot_path.at(i + 1).y) {
+                    currentLoc.y = m_robot_path.at(i + 1).y;
                 }
 
                 //calculates distance to robot
-                distanceToRobot = sqrt(pow(abs(-currentLoc.x + state.x), 2) + pow(abs(-currentLoc.y + state.y), 2));
+                distanceToRobot = sqrt(std::pow(std::fabs(-currentLoc.x + state.x), 2) + std::pow(std::fabs(-currentLoc.y + state.y), 2));
 
                 //checks to see if currentLocation is closer to the current position
                 if (distanceToRobot < shortestDistance) {
@@ -156,18 +157,18 @@ std::pair<Point2D, double> PurePursuit::get_location_on_path(const Point2D &stat
                 }
             }
                 //segment is going right
-            else if ((m_robot_path[i].y == m_robot_path[i + 1].y) && (m_robot_path[i].x < m_robot_path[i + 1].x)) {
+            else if ((m_robot_path.at(i).y == m_robot_path.at(i + 1).y) && (m_robot_path.at(i).x < m_robot_path.at(i + 1).x)) {
                 counter++;
 
                 currentLoc.x = (currentLoc.x + counter);
 
                 //if currentLocation goes beyond next point, it is set to the next point
-                if (currentLoc.x >= m_robot_path[i + 1].x) {
-                    currentLoc.x = m_robot_path[i + 1].x;
+                if (currentLoc.x >= m_robot_path.at(i + 1).x) {
+                    currentLoc.x = m_robot_path.at(i + 1).x;
                 }
 
                 //calculates distance to robot
-                distanceToRobot = sqrt(pow(abs(-currentLoc.x + state.x), 2) + pow(abs(-currentLoc.y + state.y), 2));
+                distanceToRobot = sqrt(std::pow(std::fabs(-currentLoc.x + state.x), 2) + std::pow(std::fabs(-currentLoc.y + state.y), 2));
 
                 //checks to see if currentLocation is closer to the current position
                 if (distanceToRobot < shortestDistance) {
@@ -176,18 +177,18 @@ std::pair<Point2D, double> PurePursuit::get_location_on_path(const Point2D &stat
                 }
             }
                 //segment is going down
-            else if ((m_robot_path[i].y > m_robot_path[i + 1].y) && (m_robot_path[i].x == m_robot_path[i + 1].x)) {
+            else if ((m_robot_path.at(i).y > m_robot_path.at(i + 1).y) && (m_robot_path.at(i).x == m_robot_path.at(i + 1).x)) {
                 counter--;
 
                 currentLoc.y = (currentLoc.y + counter);
 
                 //if currentLocation goes beyond next point, it is set to the next point
-                if (currentLoc.y <= m_robot_path[i + 1].y) {
-                    currentLoc.y = m_robot_path[i + 1].y;
+                if (currentLoc.y <= m_robot_path.at(i + 1).y) {
+                    currentLoc.y = m_robot_path.at(i + 1).y;
                 }
 
                 //calculates distance to robot
-                distanceToRobot = sqrt(pow(abs(-currentLoc.x + state.x), 2) + pow(abs(-currentLoc.y + state.y), 2));
+                distanceToRobot = sqrt(std::pow(std::fabs(-currentLoc.x + state.x), 2) + std::pow(std::fabs(-currentLoc.y + state.y), 2));
 
                 //checks to see if currentLocation is closer to the current position
                 if (distanceToRobot < shortestDistance) {
@@ -198,18 +199,18 @@ std::pair<Point2D, double> PurePursuit::get_location_on_path(const Point2D &stat
                 std::cout << "Current Location: " << currentLoc.x << "," << currentLoc.y << " DistanceToRobot: " << distanceToRobot << std::endl;//do not keep, for testing  DELETE
             }
                 //segment is going left
-            else if ((m_robot_path[i].y == m_robot_path[i + 1].y) && (m_robot_path[i].x > m_robot_path[i + 1].x)) {
+            else if ((m_robot_path.at(i).y == m_robot_path.at(i + 1).y) && (m_robot_path.at(i).x > m_robot_path.at(i + 1).x)) {
                 counter--;
 
                 currentLoc.x = (currentLoc.x + counter);
 
                 //if currentLocation goes beyond next point, it is set to the next point
-                if (currentLoc.x <= m_robot_path[i + 1].x) {
-                    currentLoc.x = m_robot_path[i + 1].x;
+                if (currentLoc.x <= m_robot_path.at(i + 1).x) {
+                    currentLoc.x = m_robot_path.at(i + 1).x;
                 }
 
                 //calculates distance to robot
-                distanceToRobot = sqrt(pow(abs(-currentLoc.x + state.x), 2) + pow(abs(-currentLoc.y + state.y), 2));
+                distanceToRobot = sqrt(std::pow(std::fabs(-currentLoc.x + state.x), 2) + std::pow(std::fabs(-currentLoc.y + state.y), 2));
 
                 //checks to see if currentLocation is closer to the current position
                 if (distanceToRobot < shortestDistance) {
@@ -229,5 +230,6 @@ std::pair<Point2D, double> PurePursuit::get_location_on_path(const Point2D &stat
     returnVal.second = 0;//distanceTraveled;
 
     return returnVal;
+
 }
 
